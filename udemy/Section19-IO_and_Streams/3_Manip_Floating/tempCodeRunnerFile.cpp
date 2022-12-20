be7117e94acc32de2d4fed97be4f20ea7f2f41c9
blazic <blazic016@gmail.com>
2022-12-20 21:30:11 +0100

    IO and Streams

---
+++ udemy/Section19-IO_and_Streams/3_Manip_Floating/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+    std::cout << std::setprecision(10) << std::showpoint;
+    std::cout << "-- precision 10 - showpoint ----------------------------" << std::endl;
+    std::cout << num1 << std::endl;	
+    std::cout << num2<< std::endl;	
+    std::cout << num3<< std::endl;	
