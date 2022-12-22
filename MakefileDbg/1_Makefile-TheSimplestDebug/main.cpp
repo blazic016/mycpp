0312e0f2ebd5b38da6b204edf72ad18bbd6f3441
blazic <blazic016@gmail.com>
2022-12-22 23:45:50 +0100

    MakefileDBG

---
+++ MakefileDbg/1_Makefile-TheSimplestDebug/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "sdgasgasdg" << endl;
+    cout << "sdgasgasdg" << endl;
+    cout << "sdgasgasdg" << endl;
+    cout << "sdgasgasdg" << endl;
+    cout << "sdgasgasdg" << endl;
+    cout << "sdgasgasdg" << endl;
+    cout << "sdgasgasdg" << endl;
+    cout << "sdgasgasdg" << endl;
+    cout << "sdgasgasdg" << endl;
+    cout << "sdgasgasdg" << endl;
+
+    return 0;
+}
